95379c57831c8aeb18734b5dd5213c7edbb9b1ef
Julie Kohler <julie.kohler@incontact.com>
2017-06-26 21:40:06 -0600

    IT'S ALIVE!

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import { connect } from 'react-redux'
+
+import { increment, decrement, undo, redo } from './ducks/counter'
 
 import "./App.css";
 
-export class App extends Component {
+class App extends Component {
 	render() {
+		const {
+			currentValue
+			, decrement
+			, increment
+			, futureValues
+			, previousValues
+			, undo
+			, redo
+		} = this.props; 
+		
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{ currentValue }</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={ () => increment(1) }
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={ () => increment(5) }
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={ () => decrement(1) }
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={ () => decrement(5) }
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ previousValues.length === 0 }
+							onClick={ undo }
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ futureValues.length === 0 }
+							onClick={ () => redo() }
 						>
 							Redo
 						</button>
@@ -60,4 +73,8 @@ export class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps(state) { 
+	return state 
+}
+
+export default connect(mapStateToProps, { increment, decrement, undo, redo })(App)
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,67 @@
 // change this to true to see test results on the black diamond section.
-export const BLACK_DIAMOND = false;
+export const BLACK_DIAMOND = true
+
+const initialState = {
+  currentValue: 0,
+  futureValues: [],
+  previousValues: []
+}
+
+const INCREMENT = "INCREMENT"
+, DECREMENT = "DECREMENT"
+, UNDO = "UNDO"
+, REDO = "REDO"
+
+export default function counter(state = initialState, action) {
+  switch(action.type) {
+    case "INCREMENT":
+      return {
+        currentValue: state.currentValue + action.amount
+        , futureValues: []
+        , previousValues: [state.currentValue, ...state.previousValues]
+      }
+    case "DECREMENT":
+      return {
+        currentValue: state.currentValue - action.amount
+        , futureValues: []
+        , previousValues: [state.currentValue, ...state.previousValues]
+      }
+    case "UNDO": 
+      return {
+        currentValue: state.previousValues[0]
+        , futureValues: [state.currentValue, ...state.futureValues]
+        , previousValues: state.previousValues.slice(1)
+      } 
+    case "REDO": 
+      return {
+        currentValue: state.futureValues[0]
+        , futureValues: state.futureValues.slice(1) 
+        , previousValues: [state.currentValue, ...state.previousValues]
+      }
+    default: 
+      return state
+  }
+}
+
+export function increment(amount) {
+  return {
+    amount,
+    type: INCREMENT
+  }
+}
+
+export function decrement(amount) {
+  return {
+    amount,
+    type: DECREMENT
+  }
+}
+
+export function undo() {
+  return {type: UNDO}
+}
+
+export function redo() {
+  return {type: REDO}
+}
+
